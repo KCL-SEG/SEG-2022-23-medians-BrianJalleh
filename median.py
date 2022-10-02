f2c089a0ba8e4b529b0ee7e73d394c63478785bc
Brian Jalleh <brjalleh@gmail.com>
2022-10-02 12:32:59 +0100

    Test 1

--- median.py
+++ median.py
@@ -1,12 +1,42 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
+
+
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        
+        numbers.sort()
+    
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+
+if(len(numbers)%2 == 0):
+
+    leftIndex = (int) ((len(numbers)/2)) - 1
+    rightIndex = (int) (len(numbers)/2)
+    
+    median = (numbers[leftIndex] + numbers[rightIndex])/2
+
+else:
+    #return numbers[len(numbers)/2]
+    median = numbers[(int) (len(numbers)/2)]
+    
+print(median)
+#return 1
+
+
+
+
+
+
+
+
+
